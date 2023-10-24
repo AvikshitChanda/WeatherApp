52e9431530f130cb35eb4710dacd04e12a6959f0
Avikshit Chanda <avikshitchanda@gmail.com>
2023-10-24 18:27:31 +0530

    Your commit message here

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import WeatherApp from './components/Weather App/WeatherApp';
+
+function App() {
+  return (
+    <div className="App">
+      <WeatherApp/>
+    </div>
+  );
+}
+
+export default App;
